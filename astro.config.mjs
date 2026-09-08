163ec88154b1077c6631845bfe90f8f93c161c1d
Arrosyid Al Ayubi <ayubirosyid@gmail.com>
2026-09-09 01:28:43 +0700

    update SEO and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 
+import sitemap from "@astrojs/sitemap";
+
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-    plugins: [tailwindcss()],
-  },
+  vite: {
+  plugins: [tailwindcss()],
+},
+  site: 'https://kelas5sifp.web.id',
+
+  integrations: [sitemap()],
 });
